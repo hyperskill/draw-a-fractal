233d690d64b7aeac1c75d5e7e38646fe049f2402
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-11-13 21:16:52 +0300

    Break Main into 2 classes

--- src/fractal/ApplicationRunner.java
+++ src/fractal/ApplicationRunner.java
@@ -1,7 +1,7 @@
 package fractal;
 
-public class Main {
+public class ApplicationRunner {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        new FractalDrawer();
     }
 }
---
+++ src/fractal/FractalDrawer.java
@@ -0,0 +1,11 @@
+package fractal;
+
+import javax.swing.*;
+
+public class FractalDrawer extends JFrame {
+    public FractalDrawer() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+}
