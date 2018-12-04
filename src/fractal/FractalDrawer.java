c37038f8ed0dd09a6a2828604cb26bfbe4793c3e
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-12-04 23:15:59 +0300

    stage 4: create whole pattern

--- src/fractal/FractalDrawer.java
+++ src/fractal/FractalDrawer.java
@@ -1,12 +1,14 @@
 package fractal;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FractalDrawer extends JFrame {
     public FractalDrawer() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(700, 700);
         setVisible(true);
+        getContentPane().setBackground(Color.WHITE);
         setTitle("FractalDrawer");
         add(new FractalPattern());
     }
