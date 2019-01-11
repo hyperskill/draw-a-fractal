daa759a780250b1ed8a1407558f3619ef048eef6
Sergei Khmelevskiy <xmelko91@gmail.com>
2019-01-11 10:54:14 +0300

    added deep-6

--- src/fractal/FractalDrawer.java
+++ src/fractal/FractalDrawer.java
@@ -4,15 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.concurrent.Future;
 
-public class FractalDrawer extends JFrame {
-    public FractalDrawer() throws InterruptedException {
+class FractalDrawer extends JFrame {
+    FractalDrawer(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1200, 800);
+        setSize(1800, 1000);
         DrawingPane pane = new DrawingPane(this.getWidth(), this.getHeight(), 5);
         add(pane);
-        repaint();
+        //repaint();
 
-        super.setVisible(true);
+        setVisible(true);
 
     }
 
