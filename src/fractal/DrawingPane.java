1d4d39393f828a3a49d83436779ad024158f0c18
getPL <47267794+getPL@users.noreply.github.com>
2019-02-16 00:21:02 +1000

    Update DrawingPane.java

--- src/fractal/DrawingPane.java
+++ src/fractal/DrawingPane.java
@@ -11,8 +11,27 @@ public class DrawingPane extends JComponent {
         setBounds(10, 10, 280, 280);
         Color c = new Color(ran.nextInt(256), ran.nextInt(256), ran.nextInt(256));
         g.setColor(c);
-        g.drawLine(20, 20, 100, 180);
-        g.drawLine(30, 150, 100, 200);
-        g.drawLine(90, 50, 200, 210);
+
+        int angle = 0;
+        int length = 150;
+        int xPrevious = 50;
+        int yPrevious = 10;
+        int xNow;
+        int yNow;
+
+        for (int i = 0; i < 4; i++) {
+            xNow = (int)(xPrevious + length * Math.cos(Math.toRadians(angle)));
+            yNow = (int)(yPrevious + length * Math.sin(Math.toRadians(angle)));
+            g.drawLine(xPrevious, yPrevious, xNow, yNow);
+
+            xPrevious = xNow;
+            yPrevious = yNow;
+            angle += 72;
+        }
+
+        g.drawLine(xPrevious, yPrevious, 50, 10);
+
     }
+
+
 }
